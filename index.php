fc9c53cc6d0ea5eba364b5f910cd023de08a50d0
denistrator <denistrator@yandex.ua>
2018-08-03 19:44:46 +0300

    rename validation functions

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
 
 const ALLOWED_ACTIONS = ["+", "-", "*", "/", "**"];
+const ALLOWED_COMPARISONS = ["==", "===", "!=", "<>", "!==", "<", ">", "<=", "=>"];
 
 echo 'Enter left operand:' . "\n";
 $leftOperand = getUserInput();
-checkNumericOperand($leftOperand);
+validateNumericOperand($leftOperand);
 
 echo 'Enter action ("' . implode('", "', ALLOWED_ACTIONS) . '"):' . "\n";
 $action = getUserInput();
-checkAction($action);
+validateAction($action);
 
 echo 'Enter right operand: ' . "\n";
 $rightOperand = getUserInput();
-checkNumericOperand($rightOperand);
+validateNumericOperand($rightOperand);
 
 echo 'Result: ' . calcTwoNumbers($leftOperand, $action, $rightOperand) . "\n";
 
@@ -21,14 +22,14 @@ function getUserInput()
     return trim(fgets(fopen('php://stdin', 'r')));
 }
 
-function checkAction($action)
+function validateAction($action)
 {
     if (!in_array($action, ALLOWED_ACTIONS)) {
         die('Incorrect action. Please enter correct one' . "\n");
     }
 }
 
-function checkNumericOperand($operand)
+function validateNumericOperand($operand)
 {
     if (!is_numeric($operand)) {
         die('Incorrect operand. Please enter correct one' . "\n");
